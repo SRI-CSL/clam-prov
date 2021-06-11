8cfbc6cfe3fab0b569b1d92870a33da9eca4f1bc
hassaanirshad <hassaanirshad88@gmail.com>
2021-06-11 08:07:41 -0700

    Add test for multiple tags at a call-site

---
+++ tests/test4/test.c
@@ -0,0 +1,21 @@
+void read_one(char *m){
+ *m = 'i';
+}
+
+void write_two(char *m, char *n){
+}
+
+int main(int argc, char *argv[]){
+ char i1[1], i2[1];
+ char o1[1], o2[1];
+
+ read_one(&i1[0]); // call site '0' reads 'i1'
+ read_one(&i2[0]); // call site '1' reads 'i2'
+
+ o1[0] = i1[0];
+ o2[0] = i2[0];
+
+ write_two(&o1[0], &o2[0]); // call site '2' should be dependent on call site '0', and '1'
+ // metadata on call site '2' -> 'clam-prov'={0, 1}
+ return 0;
+}
