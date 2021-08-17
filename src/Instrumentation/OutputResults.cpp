4e814229b9af698cf70c6563088d300478f112bc
caballa <navasjorgea@gmail.com>
2021-08-17 11:40:07 -0700

    fix: distinguish untracked sink from sink with no tags.
    Given a sink, the tag analysis returns whether the analysis kept track
    of all possible sources that can lead to the sink or not. If not, then
    the worst case should be assumed (i.e., all sources might reach the
    sink). When, the analysis knows about a sink it is still possible that
    the set of possible sources is empty. The meaning of this is that the
    analysis cannot connect a source with a sink. This is typically a case
    to inspect manually. Possible reasons are: a source is not recognized
    (see sources-sinks.config for all supported sources), or the source is
    defined in another library which is not available to the Tag analysis.

--- src/Instrumentation/OutputResults.cpp
+++ src/Instrumentation/OutputResults.cpp
@@ -21,31 +21,39 @@ bool TagAnalysisResultsAsMetadata(Module &M, InterGlobalClam &tagAnalysis) {
           SmallVector<long long, 4> tagVector;
 
           unsigned int argumentMetadataCount = getCallSiteArgumentMetadataCount(*CB);
+	  bool knownTags = true;
+	  bool isSink = false;
           for (int i = 0; i < argumentMetadataCount; i++) {
             MDTuple *argumentMetadata;
             if (getCallSiteArgumentMetadata(i, *CB, callId, callArg, argumentMetadata)) {
               if (getArgumentMetadataType(argumentMetadata, isInput)) {
                 if (!isInput) {
+		  isSink = true;
                   Value *Ptr = CB->getArgOperand(callArg - 1);
                   llvm::Optional<clam::ClamQueryAPI::TagVector> tags = tagAnalysis.tags(B, *Ptr);
                   if (tags.hasValue()) {
                     for (uint64_t t : tags.getValue()) {
                       tagVector.push_back(t);
                     }
-                    // DEBUG
-                    // errs() << "Tags associated to " << *CB << "={";
-                    // for (auto t: tags.getValue()) {
-                    // errs() << t << ";";
-                    // }
-                    // errs() << "}\n";
-                    ////
-                  }
+		    #if 1
+                    errs() << "Tags associated to " << *CB << "={";
+                    for (auto t: tags.getValue()) {
+                    errs() << t << ";";
+                    }
+                    errs() << "}\n";
+		    #endif 
+                  } else {
+		    knownTags = false;
+		    #if 1
+		    errs() << "No tags known for " << *CB << "\n";
+		    #endif 
+		  }
                 }
               }
             }
-          }
+	  } //end for
 
-          if (tagVector.size() > 0) {
+          if (isSink && knownTags) {
             Change = setClamProvTags(ctx, *CB, tagVector);
           }
 
